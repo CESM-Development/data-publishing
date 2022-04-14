94e98c9346670fe1e5c1f5036915a4653cfa8825
Teagan King <98482480+TeaganKing@users.noreply.github.com>
2022-04-14 12:37:44 -0600

    Update env.sh

--- config_files/LE/env.sh
+++ config_files/LE/env.sh
@@ -1,2 +1,3 @@
-export ESGINI=/usr/local/esg-publisher-config/CESM/NCAR/LE/TEST/esg-le.ini
-export ESGCESMINI=/usr/local/esg-publisher-config/CESM/NCAR/LE/TEST/esg-le.CESM.ini
+# replace 'pp' (pacific pacemaker ensemble) with a shortened name for the dataset corresponding to your directory structure
+export ESGINI=/usr/local/esg-publisher-config/CESM/LE/pp/config_files/LE/esg-le.ini
+export ESGCESMINI=/usr/local/esg-publisher-config/CESM/LE/pp/config_files/LE/esg-le.CESM.ini
